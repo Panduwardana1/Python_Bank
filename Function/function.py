4b5010b9469a12b5b730b7c148983b628382aa89
Panduwardana1 <ochaxtt@gmail.com>
2024-10-01 08:26:29 +0800

    funtion_example

---
+++ Function/function.py
@@ -0,0 +1,8 @@
+# Contoh function
+
+def perkenalan(nama,alamat,tanggal_lahir):{
+    print(f"hai nama saya {nama}, alamat saya di {alamat} dan tanggal lahir saya {tanggal_lahir}")
+
+}
+
+perkenalan("Jhony","Desa Jonyy club",2045)
